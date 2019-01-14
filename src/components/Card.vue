aa6a76231a4bc00690104dfeb3afe43a9f2c6e42
Lucas-Bech <labvs1235@gmail.com>
2019-01-14 14:59:40 +0100

    add Projects view, add Card.vue

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: ['data'],
+    data() {
+        return {
+            image: this.data.image
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+        background: url('../assets/bg_prism.png');
+        border: 2px solid black;
+    }
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,30 +1,35 @@
 <template>
     <div id="projects">
-        <div class="card">
-            
-        </div>
-        <div class="card">
-        </div>
+        <Card :data="obj" />
     </div>
 </template>
 
 <script>
+import Card from '../components/Card.vue'
 export default {
-    
+    name: 'Projects',
+    components: {
+        Card
+    },
+    data() {
+        return {
+            obj: {
+                title: "test",
+                description: "desc",
+                image: "../assets/dog.jpg"
+            }
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-#projects {
+div {
     margin: 0 5vw 0 5vw;
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(20vw, auto));
-    grid-template-rows: repeat(auto-fit, minmax(20vh, 50vh));
-    grid-row-gap: 5vh;
-    grid-column-gap: 10vh;
-    .card {
-        background: white;
-    }
+    grid-template-rows: repeat(auto-fit, minmax(auto, 50vh));
+    grid-gap: 10vh;
 }
 h1 {
     text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
